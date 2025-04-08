ce6853f3b075d12427a2e33231a1d57f9e71c480
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-09 00:06:31 +0900

    feat: add todo item with result modal

--- src/components/todo/AddComponent.js
+++ src/components/todo/AddComponent.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { postAdd } from "../../api/todoApi";
+import ResultModal from "../common/ResultModal";
+import useCustomMove from "../../hooks/useCustomMove";
 
 const initState = {
   title: "",
@@ -9,17 +12,40 @@ const initState = {
 const AddComponent = () => {
   const [todo, setTodo] = useState({ ...initState });
 
+  const [result, setResult] = useState(null);
+
+  const { moveToList } = useCustomMove();
+
   const handleChangeTodo = (e) => {
     todo[e.target.name] = e.target.value;
     setTodo({ ...todo });
   };
 
   const handleClickAdd = () => {
-    console.log(todo);
+    postAdd(todo)
+      .then((result) => {
+        setResult(result.TNO);
+        setTodo({ ...initState });
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  const closeModal = () => {
+    setResult(null);
+    moveToList();
   };
 
   return (
     <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+      {/* 모달 처리 */}
+      {result ? (
+        <ResultModal title={"Add Result"} content={`New ${result} Added`} callbackFn={closeModal} />
+      ) : (
+        <></>
+      )}
+
       <div className="flex justify-center">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <div className="w-1/5 p-6 text-right font-bold">TITLE</div>
