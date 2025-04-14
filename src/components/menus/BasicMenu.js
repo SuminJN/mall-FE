f357f1804d2de355af9f2899e3165ff65c6d5e01
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-14 19:51:56 +0900

    feat: 로그인 기능 구현

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const BasicMenu = () => {
+  const loginState = useSelector((state) => state.loginSlice);
+
   return (
     <nav id="navbar" className="flex bg-blue-300">
       <div className="w-4/5 bg-gray-500">
@@ -11,17 +14,30 @@ const BasicMenu = () => {
           <li className="pr-6 text-2xl">
             <Link to={"/about"}>About</Link>
           </li>
-          <li className="pr-6 text-2xl">
-            <Link to={"/todo/"}>Todo</Link>
-          </li>
-          <li className="pr-6 text-2xl">
-            <Link to={"/products/"}>Products</Link>
-          </li>
+
+          {loginState.email ? ( // 로그인한 사용자에게만 보여지는 메뉴
+            <>
+              <li className="pr-6 text-2xl">
+                <Link to={"/todo/"}>Todo</Link>
+              </li>
+              <li className="pr-6 text-2xl">
+                <Link to={"/products/"}>Products</Link>
+              </li>
+            </>
+          ) : (
+            <></>
+          )}
         </ul>
       </div>
 
       <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
-        <div className="text-white text-sm m-1 rounded">Login</div>
+        {!loginState.email ? (
+          <div className="text-white text-sm m-1 rounded">
+            <Link to={"/member/login"}>Login</Link>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </nav>
   );
--- src/router/root.js
+++ src/router/root.js
@@ -2,6 +2,7 @@ import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import todoRouter from "./todoRouter";
 import productsRouter from "./productsRouter";
+import memberRouter from "./memberRouter";
 
 const Loading = <div>Loading...</div>;
 const Main = lazy(() => import("../pages/MainPage"));
@@ -43,6 +44,10 @@ const root = createBrowserRouter([
       </Suspense>
     ),
     children: productsRouter(),
+  },
+  {
+    path: "member",
+    children: memberRouter(),
   },
 ]);
 
