295fd3bcb2c0cfc95fc750f2ff116eb4b3c49a98
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-28 21:49:16 +0900

    feat: update labels in ModifyComponent for clarity and remove unnecessary semicolons

--- src/components/products/ModifyComponent.js
+++ src/components/products/ModifyComponent.js
@@ -143,7 +143,6 @@ const ModifyComponent = ({ pno }) => {
           />
         </div>
       </div>
-      ;
       <div className="flex justify-center">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <div className="w-1/5 p-6 text-right font-bold">DELETE</div>
@@ -158,10 +157,9 @@ const ModifyComponent = ({ pno }) => {
           </select>
         </div>
       </div>
-      ;
       <div className="flex justify-center">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-          <div className="w-1/5 p-6 text-right font-bold">Price</div>
+          <div className="w-1/5 p-6 text-right font-bold">Files</div>
           <input
             ref={uploadRef}
             className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md resize-y"
@@ -170,7 +168,6 @@ const ModifyComponent = ({ pno }) => {
           />
         </div>
       </div>
-      ;
       <div className="flex justify-center">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <div className="w-1/5 p-6 text-right font-bold">Image</div>
